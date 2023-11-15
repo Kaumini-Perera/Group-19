c7be01e79680a5969b15e79e431d4188d55869f9
HasinduPerera2000 <147843171+HasinduPerera2000@users.noreply.github.com>
2023-11-15 17:38:25 +0530

    Resolve ModuleNotFoundError in import statements

--- vehicle_renting_system/vehicle_renting_system/asgi.py
+++ vehicle_renting_system/vehicle_renting_system/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for vehicle_reting_system project.
+ASGI config for vehicle_renting_system project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vehicle_reting_system.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vehicle_renting_system.settings')
 
 application = get_asgi_application()
--- vehicle_renting_system/vehicle_renting_system/wsgi.py
+++ vehicle_renting_system/vehicle_renting_system/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for vehicle_reting_system project.
+WSGI config for vehicle_renting_system project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vehicle_reting_system.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vehicle_renting_system.settings')
 
 application = get_wsgi_application()
