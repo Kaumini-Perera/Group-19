a8d30466a9c42a329851c0c676390c270c087de6
P.K.M. Perera <kauminipathirage2000@gmail.com>
2023-11-16 21:24:46 +0530

    Update settings.py

--- vehicle_renting_system/vehicle_renting_system/settings.py
+++ vehicle_renting_system/vehicle_renting_system/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,8 +38,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+<<<<<<< Updated upstream
     'Owner.apps.OwnerConfig',
     'Manager.apps.ManagerConfig'
+=======
+<<<<<<< Updated upstream
+=======
+    'Owner.apps.OwnerConfig',
+    'Manager.apps.ManagerConfig',
+    'CustomerHome.apps.CustomerhomeConfig',
+    'RentVehicle.apps.RentvehicleConfig',
+    'Vehicles.apps.VehiclesConfig'
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 ]
 
 MIDDLEWARE = [
@@ -124,3 +136,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# For uploading images
+MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
+MEDIA_URL = '/media/'
