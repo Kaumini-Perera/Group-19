9cc619eca2c19fdef26194ab9c506173272ab3a2
HasinduPerera2000 <147843171+HasinduPerera2000@users.noreply.github.com>
2023-11-17 17:28:44 +0530

    Added index view function

--- vehicle_renting_system/Owner/views.py
+++ vehicle_renting_system/Owner/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from Owner.models import Owner
+
+
 
 # Create your views here.
+def index(request):
+    if('user_email' not in request.session):
+        return redirect('/signin/')
+    owner_email = request.session.get('user_email')
+    owner = Owner.objects.get(Owner_email=owner_email)
+    vehicle = Vehicle.objects.all()
+    Message="Welcome Aboard!!"
+    no_of_pending_request=count_pending_rent_request()
+    return render(request,'Owner_index.html',{'vehicle':vehicle,'Message':Message,'owner':owner,'no_of_pending_request':no_of_pending_request})
