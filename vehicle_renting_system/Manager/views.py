bf5a7bf5dcf54f86ae5d6c55db49367e2d476bc6
BhagyaKarunarathna <147934231+BhagyaKarunarathna@users.noreply.github.com>
2023-11-18 22:49:50 +0530

    Remove updates to the views.py; Manager

--- vehicle_renting_system/Manager/views.py
+++ vehicle_renting_system/Manager/views.py
@@ -1,14 +1,1 @@
-from django.shortcuts import redirect, render
-from Manager.models import Manager
-from Vehicles.models import Vehicle
 
-# Create your views here.
-def index(request):
-    if('user_email' not in request.session):
-        return redirect('/signin/')
-    manager_email = request.session.get('user_email')
-    manager = Manager.objects.get(Manager_email=manager_email)
-    vehicle = Vehicle.objects.all()
-    Message="Welcome Aboard!!"
-    no_of_pending_request=count_pending_rent_request()
-    return render(request,'Manager_index.html',{'vehicle':vehicle,'Message':Message,'manager':manager,'no_of_pending_request':no_of_pending_request})
