a95732f288369419930068d085ee09ec23a3e6ab
BhagyaKarunarathna <147934231+BhagyaKarunarathna@users.noreply.github.com>
2023-11-19 12:23:57 +0530

    Update view.py in the app; Manager
    Define the index page of a Manager

--- vehicle_renting_system/Manager/views.py
+++ vehicle_renting_system/Manager/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+from Owner.models import Owner
 from Manager.models import Manager
+from CustomerHome.models import Customer
 from Vehicles.models import Vehicle
+from RentVehicle.models import RentVehicle
+
+from datetime import datetime
+from datetime import date
 
 # Create your views here.
 def index(request):
