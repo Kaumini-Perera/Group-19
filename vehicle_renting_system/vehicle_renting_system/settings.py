015be0fac47ad7d14fd060df6fe4364cb9452a83
P.K.M. Perera <kauminipathirage2000@gmail.com>
2023-11-27 08:41:39 +0530

    Settings change

--- vehicle_renting_system/vehicle_renting_system/settings.py
+++ vehicle_renting_system/vehicle_renting_system/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'vehicle_renting_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates'), os.path.join(BASE_DIR,'static')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
